041924560d82260da5dee9c7d286e3e79debfdea
sumon <sumoncpi10@gmail.com>
2022-03-06 21:49:59 +0600

    ok

--- js/phone.js
+++ js/phone.js
@@ -11,26 +11,69 @@ const searchPhones= () =>{
     searchField.value="";
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySearchResult(data.data));
+    .then(data => displaySearchResult(data));
+    // .then(data => console.log(data));
 }
 
 const displaySearchResult = phones=>{
-    // console.log(phones);
+    console.log(phones);
     const searchResult=document.getElementById('search-result');
-    phones.forEach(phone => {
-        console.log(phone);
+    if(phones.status){
+        
+        phones.data.forEach(phone => {
+            console.log(phone);
+            const div=document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML=
+            `
+            <div class="card" onclick=phoneDetail("${phone.slug}")>
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title text-center">Card ${phone.phone_name}</h5>
+                </div>
+            </div>
+            `;
+                searchResult.appendChild(div);
+        });
+    }
+    else{
+        alert("No phone Found!!");
+    }
+}
+
+const phoneDetail= phone =>{
+    console.log(phone);
+    // console.log(phone);
+   
+    const url=`https://openapi.programming-hero.com/api/phone/${phone}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => phoneDetails(data.data));
+}
+const phoneDetails= phndtl=>{
+    console.log(phndtl);
+    const detail=document.getElementById('phone-detail');
+    detail.innerText="";
+   
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=
         `
-        <div class="card">
-         <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card mb-3">
+        <img src="${phndtl.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card ${phone.phone_name}</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${phndtl.name}</h5>
+          <p class="card-text"><small class="text-muted">${phndtl.releaseDate}</small></p>
+
+          <h6 class="card-title">storage: ${phndtl.mainFeatures.storage}</h6>
+          <h6 class="card-title">chipSet: ${phndtl.mainFeatures.chipSet}</h6>
+          <h6 class="card-title">displaySize:${phndtl.mainFeatures.displaySize}</h6>
+          <h6 class="card-title">memory:${phndtl.mainFeatures.memory}</h6>
+          <p class="card-text"></p>
+          
         </div>
       </div>
         `;
-        searchResult.appendChild(div);
-   });
+            detail.appendChild(div);
+   
 }
